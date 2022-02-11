c97dd1f1e1cba9b774612cc1c874b3e2c51e9880
Bryan Garcia <bryan.ago99@gmail.com>
2022-02-11 15:12:37 -0700

    Tittle on .html files updated, time added to greetings methods

--- src/main/java/bryan/example/SpringBootPractice/controller/GreetingsController.java
+++ src/main/java/bryan/example/SpringBootPractice/controller/GreetingsController.java
@@ -5,34 +5,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class GreetingsController {
+    DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
 
     @GetMapping("/")
     public String index (Model model){
-        String mainGreeting = "Hello user, you visited this page at: " + LocalDate.now().toString();
+        String mainGreeting = "Hello user, you visited this page at: " + LocalDate.now().toString() + " at: " + LocalDateTime.now().format(timeFormat).toString();
         model.addAttribute("greeting",mainGreeting);
         return "index";
     }
 
     @GetMapping("/Spanish")
     public String spanishGreeting (Model model){
-        String mainGreeting = "Hola usuario, visitaste esta página el: " + LocalDate.now().toString();
+        String mainGreeting = "Hola usuario, visitaste esta página el: " + LocalDate.now().toString() + " a las: "+ LocalDateTime.now().format(timeFormat).toString();
         model.addAttribute("greeting",mainGreeting);
         return "spanishGreeting";
     }
 
     @GetMapping("/French")
     public String frenchGreeting (Model model){
-        String mainGreeting = "Bonjour utilisateur, vous avez visité cette page sur : " + LocalDate.now().toString();
+        String mainGreeting = "Bonjour utilisateur, vous avez visité cette page sur : " + LocalDate.now().toString()  + " à: "+ LocalDateTime.now().format(timeFormat).toString();
         model.addAttribute("greeting",mainGreeting);
         return "frenchGreeting";
     }
 
     @GetMapping("/Italian")
     public String italianGreeting (Model model){
-        String mainGreeting = "Ciao utente, hai visitato questa pagina su:: " + LocalDate.now().toString();
+        String mainGreeting = "Ciao utente, hai visitato questa pagina su: " + LocalDate.now().toString() +" in: "+ LocalDateTime.now().format(timeFormat).toString() ;
         model.addAttribute("greeting",mainGreeting);
         return "italianGreeting";
     }
